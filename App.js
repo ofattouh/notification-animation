a2ad8a5dcb62c7caa2f4775e5919d83e991f5bdb
omar <omohamed@pshsa.ca>
2021-01-08 15:24:06 -0500

    custom notifiation app

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, TouchableOpacity, StyleSheet, View, SafeAreaView } from 'react-native';
+import Notification from './Notification';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    notify: false,
+    message: 'This message will close after 3 seconds...',
+  };
+
+  toggleNotification = () => {
+    this.setState({ notify: !this.state.notify });
+  }
+
+  render() {
+    const notify = this.state.notify ?
+      <Notification autoHide message={this.state.message} onClose={this.toggleNotification} /> : null;
+
+    return (
+      <SafeAreaView>
+        <View style={styles.content}>
+          <TouchableOpacity onPress={this.toggleNotification} style={styles.btn}>
+            <Text style={styles.text}>Show message</Text>
+          </TouchableOpacity>
+          {notify}
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  content: {
+    padding: 10,
+    overflow: 'hidden',
   },
+  btn: {
+    margin: 30,
+    backgroundColor: '#9b59b6',
+    borderRadius: 3,
+    padding: 10,
+    width: 200,
+  },
+  text: {
+    textAlign: 'center',
+    color: '#fff',
+  }
 });
+
+// expo init my-app
+// https://reactnative.dev/docs/animated
+// https://reactnative.dev/docs/easing
---
+++ Notification/index.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import { Animated, Easing, StyleSheet, Text } from 'react-native';
+
+export default class Notification extends Component {
+  constructor(props){  
+    super(props);
+    
+    // animatedValue will be used as the value for opacity. Initial Value: 0
+    this.animatedValue = new Animated.Value(0); 
+  }
+
+  static defaultProps = {
+    delay: 3000,
+    // onOpen: () => {},
+    onClose: () => {},
+  };
+
+  state = {
+    height: -1000,
+  };
+
+  componentDidMount() {
+    this.startSlideIn();
+  }
+
+  getAnimation(value, autoHide) {
+    const { delay } = this.props;
+
+    return Animated.timing( 
+      this.animatedValue,
+      {
+        toValue: value,
+        duration: 500,
+        easing: Easing.cubic,
+        delay: autoHide ? delay : 0,
+        useNativeDriver: false,
+      }
+    );
+  }
+
+  // from 0 to 1
+  startSlideIn () {
+    const { autoHide } = this.props;
+    this.animatedValue.setValue(0);
+
+    this.getAnimation(1)
+      .start(() => {
+        if (autoHide){
+          this.startSlideOut();
+        }
+      });
+  }
+
+  // from 1 to 0
+  startSlideOut() {
+    const { autoHide, onClose } = this.props;
+
+    this.animatedValue.setValue(1);
+    this.getAnimation(0, autoHide).start(() => onClose());
+  }
+
+  onLayoutChange = (event) => {
+    const { layout: { height } } = event.nativeEvent;
+
+    // only re-render component once for performance reasons
+    if (this.state.height === -1000) {
+      this.setState({ height });
+    }
+  }
+
+  render() {
+    const { message } = this.props;
+    const { height } = this.state;
+
+    const top = this.animatedValue.interpolate({
+      inputRange: [0, 1], // ascending
+      outputRange: [-height, 0],
+    });
+
+    return (
+      <Animated.View onLayout={this.onLayoutChange} style={[ styles.main, { top } ]}>
+        <Text style={styles.text}>{message}</Text>
+      </Animated.View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  main: {
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    padding: 20,
+    position: 'absolute',
+    left: 0,
+    right: 0,
+  },
+  text: {
+    color: '#fff',
+  },
+});
